b0e84c1644bdd0cf48faa6ce78d6cfef316ded05
sines05 <160707657+sines05@users.noreply.github.com>
2024-10-21 17:31:03 +0700

    Create some packages

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.library_management {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.library_management to javafx.fxml;
-    exports org.example.library_management;
-}
