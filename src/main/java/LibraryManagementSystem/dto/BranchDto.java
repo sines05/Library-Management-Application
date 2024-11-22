4f7c5ee7dd8ea86d0fc49b8c08b74f971d3ede94
Lenovo <sonnguyenque5@gmail.com>
2024-11-22 21:50:12 +0700

    Update some controller

---
+++ src/main/java/LibraryManagementSystem/dto/BranchDto.java
@@ -0,0 +1,18 @@
+package LibraryManagementSystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BranchDto {
+
+    private int id;
+    private String name;
+    private String location;
+    private String contactNo;
+    private AdminDto admin;
+
+}
---
+++ src/main/java/LibraryManagementSystem/dto/TransactionDto.java
@@ -0,0 +1,24 @@
+package LibraryManagementSystem.dto;
+
+import LibraryManagementSystem.entity.TransactionDetail;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TransactionDto {
+
+    private int id;
+    private String transactionType;
+    private int bookQty;
+    private String dueDate;
+    private Timestamp dateAndTime;
+    private List<TransactionDetail> transactionDetails;
+    private UserDto user;
+
+}
